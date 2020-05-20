bb57fb547c749d619f79293f2b33db30a0869e01
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-05-20 19:37:54 -0300

    agrego colores a createComponent

--- createComponent.sh
+++ createComponent.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 # ingresar npm run <nombre quee le pongas al script en el package>  c (para component)  nombreComponent  o npm run s (para screen) nombreScreen ejemplo: npm run component c mycomponent 
 
+COLOR_ERROR='\033[1;91m';
+COLOR_SUCCESS='\033[1;92m';
+
 function createComponent {
   dir=$1;
   component=${2^};
   route=src/$dir/$component;
- 
+  
   if [ -d $route ];
     then
-      echo "$component ya existe en $route";
+      echo -e "\n${COLOR_ERROR}Error $component ya existe en $route\n";
       exit 1;
     else
       mkdir -p $route;
       touch $route/index.js $route/index.module.scss
       echo -e "import React from 'react';\n\nimport styles from './index.module.scss';\n\nexport default function $component() {
       return <h1>$component</h1>;\n}" > $route/index.js;
-      echo "** $dir  $component creado en $route/$component **";
+      echo -e "\n${COLOR_SUCCESS}$component creado con exito en $route/$component\n";
   fi
 }
 
 if [[ ${1} != '-c' && ${1} != '-s'  || ${2} == ''  ]]; 
   then  
-    echo "PARAMETROS INCORRECTOS";
+    echo -e "\n${COLOR_ERROR}PARAMETROS INCORRECTOS\n";
     exit 1;
 fi
 
