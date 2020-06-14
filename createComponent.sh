666fee7b23b022066c30b954a12e932327b388da
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-06-13 21:37:39 -0300

    borro archivos y corrijo saltos de linea

--- createComponent.sh
+++ createComponent.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # ingresar npm run <nombre quee le pongas al script en el package>  c (para component)  nombreComponent  o npm run s (para screen) nombreScreen ejemplo: npm run component c mycomponent 
+# if you use it  as script  in  your package.json enter npm run <name-script> <c> <component-names> or npm run <s> <screen-names>. 
+# Example: npm run cc c my-component
 
 COLOR_ERROR='\033[1;91m';
 COLOR_SUCCESS='\033[1;92m';
@@ -30,7 +32,7 @@ function createComponents() {
  
  if [[ ${components} == '' ]];
   then
-   echo -e "${COLOR_ERROR}${0} require one or more components/screen names.\nTry ${0} 'help' for more information";
+   echo -e "${COLOR_ERROR}${0} require one or more components/screen names.\nTry ${0} 'help' for more information.";
    exit 1;
   fi
 
@@ -46,6 +48,6 @@ case "$1" in
     c)createComponents components $@;;
     s)createComponents screens $@;;
     help) echo -e "OPTIONS:\n 1) c <component-name>\n 2) s <screen-name>";;
-    *) echo -e "${COLOR_ERROR}Invalid option -- '${1}'. Try 'help' for more information";;
+    *) echo -e "${COLOR_ERROR}Invalid option -- '${1}'.\nTry 'help' for more information";;
 esac
 
